d58f412961a1ad1e8938871f7dc43449a074dbd0
Nurik <tanatov1989@mail.ru>
2017-12-13 17:57:04 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp23/Forex.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp23
+{
+   public  class Forex
+   {
+        public string Oblegation { get;  set; }
+        public int Price { get; set; }
+        public int Income { get; set; }
+        public int tax;
+        public string[] mas = new string[3];
+        Random rand = new Random();
+        public Forex()
+        {
+            Price = 0;
+            mas[0] = "Tele 2";
+            mas[1] = "KazKom";
+            mas[2] = "Beeline";
+        }
+        public void TaxFromIncome(int procent,int price)
+        {
+            
+            Income = price - (price / 100) * procent;
+            WriteLine(Income);
+            WriteLine(procent);
+            WriteLine(price);
+
+        }
+        public void RandAndShowOblegation(int Price1, int Price2, int Price3)
+        {
+            Dictionary<string, int> oblegation = new Dictionary<string, int>();
+           oblegation.Add(mas[0],Price1);
+            oblegation.Add(mas[1],Price2);
+            oblegation.Add(mas[2],Price3);
+            int i = 0;
+            foreach (var os in oblegation)
+            {
+               
+                Write(i+" ");
+                WriteLine(os.Key + " - " + os.Value);
+                i++;
+            }
+        }
+   }
+}
---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int price = 0;
+            int price2 = 0;
+            int price3 = 0;
+            int procent = 10;
+            int income = 0;
+            int choosen = 0;
+            Random rand = new Random();
+            Person person = new Person();
+            Forex forex = new Forex();
+            string surnameOfPerson = " ";
+            string nameOfPerson = " ";          
+            WriteLine("Введите свое имя");
+            nameOfPerson = ReadLine();
+            person.Name = nameOfPerson;
+            WriteLine("Введите свою фамилия");
+            surnameOfPerson = ReadLine();
+            person.SurName = surnameOfPerson;
+            price=rand.Next(100000);
+            price2 = rand.Next(100000);
+            price3 = rand.Next(100000);
+            //forex.Price = price;
+            forex.RandAndShowOblegation(price,price2,price3);
+            WriteLine("Выберите акцию");
+            while (!int.TryParse(ReadLine(), out choosen))
+            {
+                Clear();
+                Write("Ошибка, введите целое число: ");
+            }
+            if(choosen==0)
+            {
+                forex.TaxFromIncome(procent, price);
+            }
+            else if (choosen == 1)
+            {
+                forex.TaxFromIncome(procent, price2);
+            }
+            else if (choosen == 2)
+            {
+                forex.TaxFromIncome(procent, price3);
+            }
+            ReadLine();
+
+        }
+    }
+}
